3d4e0325a3fb981d68d7af108aea2fa5ca57566b
Viktor Renkema <vrenkema@gmail.com>
2023-08-10 21:16:48 +0200

    Add meta tag

--- components/meta.tsx
+++ components/meta.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function Meta() {
   return (
     <Head>
-      <title>Viktor Renkema | Software developer</title>
+      <title>Viktor | Front-end developer</title>
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
@@ -11,7 +11,33 @@ export default function Meta() {
         👋
       </text>
     </svg>"
+      ></link>
+      <meta name="title" content="I’m looking for a new front-end role!" />
+      <meta
+        name="description"
+        content="I’m looking for a new front-end role!"
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.viktorrenkema.com/" />
+      <meta property="og:title" content="Viktor | Front-end developer" />
+      <meta
+        property="og:description"
+        content="I’m looking for a new front-end role!"
+      />
+      <meta property="og:image" content="/images/meta-image.png" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@vrenkema" />
+      <meta name="twitter:title" content="Viktor | Front-end developer" />
+      <meta
+        name="twitter:description"
+        content="I’m looking for a new front-end role!"
+      />
+      <meta name="twitter:image:src" content="/images/meta-image.png" />
     </Head>
   );
 }
